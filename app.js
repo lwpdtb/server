f4fbefb7be11bdcdb0d2779c7e437d6e69d2fc21
lwpdtb <2364357112@qq.com>
2018-03-28 17:05:01 +0800

    解决了简单的消息互传不通的问题

--- app.js
+++ app.js
@@ -93,9 +93,22 @@ var numUsers = 0;
 
 
 io.on('connection', function (socket) {
-  console.log('log')
+  // 当客户端发出“new message”时，服务端监听到并执行相关代码
+  socket.on('new message', function (data) {
+      // 广播给用户执行“new message”   
+      console.log(data)
+      io.sockets.emit('new message', data);
+  });
   
+  // 当客户端发出“add user”时，服务端监听到并执行相关代码
+  // socket.on('add user', function (username) {
+  //     socket.username = username;
+  //     //服务端告诉当前用户执行'login'指令
+  //     socket.emit('login', {});
+  // });
+  
+  // 当用户断开时执行此指令
+  socket.on('disconnect', function () {});
 });
-
 
 module.exports = app;
